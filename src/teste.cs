3d37589556625566de170558e2fafa8a61142d75
Matheus Castilho <matheusc.pavam@gmail.com>
2026-06-24 23:27:50 -0300

    Initial commit - compiladorCachacaScript

---
+++ src/teste.cs
@@ -0,0 +1,16 @@
+abreAButelada
+
+alambique doseInteira soma abreCopo doseInteira a maisUmaDose doseInteira b fechaCopo
+abreBarril
+devolveDose a + b viraDose
+fechaBarril
+
+abreBarril
+
+doseInteira resultado viraDose
+resultado engarrafar soma abreCopo 5 maisUmaDose 3 fechaCopo viraDose
+
+serveNoCopo abreCopo resultado fechaCopo viraDose
+
+fechaBarril
+fechaAButelada
